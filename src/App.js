f2f840436cc120d5fa008039ea9ab0079bd66db3
Meraj <ansmj123@gmail.com>
2022-01-06 14:50:25 +0530

    create images and service page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./Component/Image";
+function App() {
+  return (
+    <div className="App">
+      <Image />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Image.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Image = () => {
+  return <div>
+      <h1>Images</h1>
+  </div>;
+};
+export default Image;
